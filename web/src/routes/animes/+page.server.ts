82bdd0bb35f828a78c65ff77a2d9dd76197babc7
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-05-19 21:06:41 +0200

    Frontend: Added pages

--- web/src/routes/animes/+page.server.ts
+++ web/src/routes/animes/+page.server.ts
@@ -1,8 +1,11 @@
+import { getPagedQueryOptions } from "$lib/utils";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ locals }) => {
-  const animes = await locals.apiClient.getAnimes();
+export const load: PageServerLoad = async ({ locals, url }) => {
+  const query = getPagedQueryOptions(url.searchParams);
+
+  const animes = await locals.apiClient.getAnimes({ query });
   if (!animes.success) {
     throw error(animes.error.code, { message: animes.error.message });
   }
--- web/src/routes/animes/+page.svelte
+++ web/src/routes/animes/+page.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
-  import { invalidateAll } from "$app/navigation";
-  import { getApiClient, handleApiError } from "$lib";
+  import { goto } from "$app/navigation";
+  import { getApiClient } from "$lib";
   import Image from "$lib/components/Image.svelte";
   import Spacer from "$lib/components/Spacer.svelte";
-  import { cn, pickTitle } from "$lib/utils.js";
-  import {
-    Button,
-    buttonVariants,
-    Checkbox,
-    Dialog,
-    Input,
-    Label,
-  } from "@nanoteck137/nano-ui";
-  import { Eye, Star } from "lucide-svelte";
-  import toast from "svelte-5-french-toast";
+  import { pickTitle } from "$lib/utils.js";
+  import { Pagination } from "@nanoteck137/nano-ui";
+  import { Star } from "lucide-svelte";
   import ImportMalListDialog from "./ImportMalListDialog.svelte";
   import ImportAnime from "./ImportAnime.svelte";
+  import { page } from "$app/stores";
 
   const { data } = $props();
   const apiClient = getApiClient();
@@ -28,6 +21,8 @@
 
 <ImportMalListDialog />
 <ImportAnime />
+
+<Spacer size="sm" />
 
 <div class="flex flex-col gap-4">
   {#each data.animes as anime}
@@ -54,3 +49,46 @@
     </div>
   {/each}
 </div>
+
+<Spacer size="sm" />
+
+<Pagination.Root
+  page={data.page.page + 1}
+  count={data.page.totalItems}
+  perPage={data.page.perPage}
+  siblingCount={1}
+  onPageChange={(p) => {
+    const query = $page.url.searchParams;
+    query.set("page", (p - 1).toString());
+
+    goto(`?${query.toString()}`, { invalidateAll: true, keepFocus: true });
+  }}
+>
+  {#snippet children({ pages, currentPage })}
+    <Pagination.Content>
+      <Pagination.Item>
+        <Pagination.PrevButton />
+      </Pagination.Item>
+      {#each pages as page (page.key)}
+        {#if page.type === "ellipsis"}
+          <Pagination.Item>
+            <Pagination.Ellipsis />
+          </Pagination.Item>
+        {:else}
+          <Pagination.Item>
+            <Pagination.Link
+              href="?page={page.value}"
+              {page}
+              isActive={currentPage === page.value}
+            >
+              {page.value}
+            </Pagination.Link>
+          </Pagination.Item>
+        {/if}
+      {/each}
+      <Pagination.Item>
+        <Pagination.NextButton />
+      </Pagination.Item>
+    </Pagination.Content>
+  {/snippet}
+</Pagination.Root>
