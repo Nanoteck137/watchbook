17e724d68290b8dbc97c95ecf0b7d0ad513dc042
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-09-03 18:48:19 +0200

    Removed test code

--- cmd/watchbook/cmd/serve.go
+++ cmd/watchbook/cmd/serve.go
@@ -1,41 +1,11 @@
 package cmd
 
 import (
-	"context"
-	"time"
-
 	"github.com/nanoteck137/watchbook/apis"
 	"github.com/nanoteck137/watchbook/config"
 	"github.com/nanoteck137/watchbook/core"
-	"github.com/nanoteck137/watchbook/database"
 	"github.com/spf13/cobra"
 )
-
-func test(app core.App) error {
-	testId := "cka7s522p9zt"
-
-	{
-		// t := time.Now()
-		// t = t.AddDate(0, 0, 0)
-
-		// t, _ := time.Parse(time.RFC3339, "2025-07-03T16:00:00Z")
-		// t, _ := time.Parse(time.RFC3339, "2025-09-03T2:00:00Z")
-		t, _ := time.Parse(time.RFC3339, "2025-09-03T00:00:00+02:00")
-
-		err := app.DB().SetMediaPartRelease(context.Background(), testId, database.SetMediaPartRelease{
-			StartDate:        t.Format(time.RFC3339),
-			NumExpectedParts: 12,
-			PartOffset:       11,
-			IntervalDays:     7,
-			DelayDays:        0,
-		})
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
 
 var serveCmd = &cobra.Command{
 	Use: "serve",
@@ -45,11 +15,6 @@ var serveCmd = &cobra.Command{
 		err := app.Bootstrap()
 		if err != nil {
 			app.Logger().Fatal("Failed to bootstrap app", "err", err)
-		}
-
-		err = test(app)
-		if err != nil {
-			app.Logger().Fatal("failed to run test", "err", err)
 		}
 
 		e, err := apis.Server(app)
