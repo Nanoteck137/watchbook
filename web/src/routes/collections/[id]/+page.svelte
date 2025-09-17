ca4847e0e431db358633434ddf57361f4f311fd3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-09-18 01:01:15 +0200

    Fixing the layout when images are missing, fixes #79

--- web/src/routes/collections/[id]/+page.svelte
+++ web/src/routes/collections/[id]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import MediaCard from "$lib/components/MediaCard.svelte";
   import { Button, buttonVariants } from "@nanoteck137/nano-ui";
-  import { Image as ImageIcon, Plus } from "lucide-svelte";
+  import { FileQuestion, Image as ImageIcon, Plus } from "lucide-svelte";
   import ShowLogoModal from "./ShowLogoModal.svelte";
   import { cn, isRoleAdmin } from "$lib/utils";
   import { getApiClient, handleApiError } from "$lib";
@@ -12,40 +12,42 @@
   import EditImagesModal from "./EditImagesModal.svelte";
   import CollectionDropdown from "./CollectionDropdown.svelte";
   import Spacer from "$lib/components/Spacer.svelte";
-  import ProviderUpdateModal from "./ProviderUpdateModal.svelte";
 
   const { data } = $props();
   const apiClient = getApiClient();
 
   let openAddMediaModal = $state(false);
   let openEditImagesModal = $state(false);
-  let openUpdateModal = $state(false);
 </script>
 
 <div
   class="relative h-48 w-full overflow-hidden rounded-lg shadow-lg sm:h-60 md:h-72"
 >
-  <img
-    src={data.collection.bannerUrl}
-    alt="Collection Banner"
-    class="h-full w-full object-cover"
-  />
+  {#if data.collection.bannerUrl}
+    <img
+      src={data.collection.bannerUrl}
+      alt="Collection Banner"
+      class="h-full w-full object-cover"
+    />
+  {:else}
+    <div class="h-full w-full bg-background"></div>
+  {/if}
 
-  <div class="absolute inset-0 bg-black bg-opacity-40"></div>
+  <div class="absolute inset-0 rounded-lg border bg-black bg-opacity-40"></div>
 
-  <div
-    class="pointer-events-none absolute inset-0 hidden items-center justify-center md:flex"
-  >
+  {#if data.collection.logoUrl}
     <div
-      class="w-32 rounded-lg border border-gray-700 bg-gray-900 bg-opacity-70 p-4 shadow-lg sm:w-40 md:w-48"
+      class="pointer-events-none absolute inset-0 hidden items-center justify-center md:flex"
     >
-      <img
-        src={data.collection.logoUrl}
-        alt="Collection Logo"
-        class="mx-auto max-h-24 w-auto object-contain"
-      />
+      <div class="w-40 rounded-lg border bg-black bg-opacity-60 p-4 shadow-lg">
+        <img
+          src={data.collection.logoUrl}
+          alt="Collection Logo"
+          class="mx-auto max-h-24 w-auto object-contain"
+        />
+      </div>
     </div>
-  </div>
+  {/if}
 
   {#if data.collection.logoUrl}
     <ShowLogoModal
@@ -65,13 +67,19 @@
   class="relative mx-2 -mt-16 flex flex-col items-center space-y-4 px-4 sm:-mt-20 sm:flex-row sm:items-start sm:space-x-6 sm:space-y-0 sm:px-0"
 >
   <div
-    class="relative z-10 aspect-[75/106] w-40 min-w-40 flex-shrink-0 overflow-hidden rounded-lg border-4 border-gray-900 bg-gray-800 shadow-lg sm:w-48 sm:min-w-48"
+    class="relative z-10 aspect-[75/106] w-40 min-w-40 flex-shrink-0 overflow-hidden rounded-lg border bg-black shadow-lg sm:w-48 sm:min-w-48"
   >
-    <img
-      src={data.collection.coverUrl}
-      alt="Collection Cover"
-      class="h-full w-full object-cover"
-    />
+    {#if data.collection.coverUrl}
+      <img
+        src={data.collection.coverUrl}
+        alt="Collection Cover"
+        class="h-full w-full object-cover"
+      />
+    {:else}
+      <div class="flex h-full w-full items-center justify-center">
+        <FileQuestion size={52} />
+      </div>
+    {/if}
 
     <CollectionDropdown collection={data.collection} />
   </div>
@@ -82,56 +90,56 @@
     <h1 class="text-2xl font-bold drop-shadow-lg sm:pt-2">
       {data.collection.name}
     </h1>
-    <!-- <p class="mt-2 text-lg text-gray-300">
-        All seasons and movies of the Fullmetal Alchemist franchise, including
-        Brotherhood and the original series.
-      </p>
-      <p class="mt-4 text-sm italic text-gray-400">
-        Updated regularly with new releases and extras.
-      </p> -->
+    <!-- <p class="text-gray-300">
+      All seasons and movies of the Fullmetal Alchemist franchise, including
+      Brotherhood and the original series.
+    </p>
+    <p class="text-sm italic text-gray-400">
+      Updated regularly with new releases and extras.
+    </p> -->
   </div>
 </div>
 
-<div class="mx-auto max-w-7xl py-12">
-  <div class="flex items-center justify-between">
-    <h2 class="text-bold text-xl">
-      Collection Items
-      {#if isRoleAdmin(data.user?.role)}
-        <Button
-          variant="ghost"
-          size="icon"
-          onclick={() => {
-            openAddMediaModal = true;
-          }}
-        >
-          <Plus />
-        </Button>
-      {/if}
-    </h2>
-    <p class="text-sm">{data.items.length} item(s)</p>
-  </div>
+<Spacer size="md" />
 
-  <Spacer size="md" />
+<div class="flex items-center justify-between">
+  <h2 class="text-bold text-xl">
+    Collection Items
+    {#if isRoleAdmin(data.user?.role)}
+      <Button
+        variant="ghost"
+        size="icon"
+        onclick={() => {
+          openAddMediaModal = true;
+        }}
+      >
+        <Plus />
+      </Button>
+    {/if}
+  </h2>
+  <p class="text-sm">{data.items.length} item(s)</p>
+</div>
 
-  <div
-    class="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] items-center justify-items-center gap-6"
-  >
-    {#each data.items as item}
-      <div class="relative">
-        <MediaCard
-          href="/media/{item.mediaId}"
-          coverUrl={item.coverUrl}
-          title={item.collectionName}
-          startDate={item.startDate}
-          partCount={item.partCount}
-          score={item.score}
-          userList={item.user?.list ?? null}
-        />
+<Spacer size="md" />
 
-        <MediaItemDropdown {item} />
-      </div>
-    {/each}
-  </div>
+<div
+  class="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] items-center justify-items-center gap-6"
+>
+  {#each data.items as item}
+    <div class="relative">
+      <MediaCard
+        href="/media/{item.mediaId}"
+        coverUrl={item.coverUrl}
+        title={item.collectionName}
+        startDate={item.startDate}
+        partCount={item.partCount}
+        score={item.score}
+        userList={item.user?.list ?? null}
+      />
+
+      <MediaItemDropdown {item} />
+    </div>
+  {/each}
 </div>
 
 <AddMediaItem
