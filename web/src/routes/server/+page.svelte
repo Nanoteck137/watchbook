1f0b02c88cf287238b1330ae089f611fd856fb4b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-09-21 00:47:39 +0200

    Added Providers button to /server

--- web/src/routes/server/+page.svelte
+++ web/src/routes/server/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
   import { PUBLIC_COMMIT, PUBLIC_VERSION } from "$env/static/public";
-  import { getApiClient, handleApiError } from "$lib";
   import { Button } from "@nanoteck137/nano-ui";
-  import { onDestroy, onMount } from "svelte";
-  import { z } from "zod";
-
-  const { data } = $props();
-  const apiClient = getApiClient();
 </script>
 
 <p>Server Page (W.I.P)</p>
 
 <p>Version: {PUBLIC_VERSION}</p>
 <p>Commit: {PUBLIC_COMMIT}</p>
+
+<Button href="/server/providers">Providers</Button>
